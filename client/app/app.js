e8c0937d7fbc85549d35576844d9d4987813350e
Khaled Ali <alikkhaledali@gmail.com>
2016-12-05 09:34:26 -0500

    Feature-bb-setup: Initial set up, Wrote CSS for the nave bar and app container

--- client/app/app.js
+++ client/app/app.js
@@ -10,11 +10,42 @@ angular.module('app', [
     Common,
     Components
   ])
-  .config(($locationProvider) => {
+  .config(($locationProvider, $stateProvider, $urlRouterProvider) => {
     "ngInject";
     // @see: https://github.com/angular-ui/ui-router/wiki/Frequently-Asked-Questions
     // #how-to-configure-your-server-to-work-with-html5mode
     $locationProvider.html5Mode(true).hashPrefix('!');
+
+    // The app routing is defined here. We will keep our layout inside the app component
+    // The layout route will be abstract and it will hold all of the views
+    $stateProvider
+      .state('app', {
+        url: '/app',
+        abstract: true,
+        template: '<app><app>'
+      }) 
+
+      // Dashboard page to contain our vehicles info. list page
+      .state('app.home', {
+        url: '/home',
+        template: 'Home Paes'
+      })
+
+      // Vechiles info search page
+      .state('app.search', {
+        url: '/search',
+        template: 'Vechiles Search page'
+      })
+
+
+      // Vechiles info search page
+      .state('app.about', {
+        url: '/about',
+        template: '<about></about>'
+      });
+
+
+    $urlRouterProvider.otherwise('/app/home');
   })
 
   .component('app', AppComponent);
--- client/app/components/components.js
+++ client/app/components/components.js
@@ -1,9 +1,7 @@
 import angular from 'angular';
-import Home from './home/home';
 import About from './about/about';
 
 let componentModule = angular.module('app.components', [
-  Home,
   About
 ])
   
